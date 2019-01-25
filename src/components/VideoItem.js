692ade518800a82a8edc77c8e90a674732a4203f
Gabe Velasquez <gvelasquez@trackforce.com>
2019-01-24 17:38:57 -0800

    css changes on each videoitem

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,14 @@
 import React from "react";
 import VideoList from "./VideoList";
+import "./VideoItem.css";
 
 const VideoItem = ({ video }) => {
   return (
-    <div className="item">
-      <img src={video.snippet.thumbnails.medium.url} />
-      {video.snippet.title}
+    <div className="video-item item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
     </div>
   );
 };
